f73772f810e965e9418deff56775b95b89523350
Vasyl Diachuk <djachukvasja@gmail.com>
2024-01-26 15:57:37 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
-
+console.log(document.body.clientWidth);
+const exercisesTitle = document.querySelector(`.exercises-title-span`);
 const exercisesList = document.querySelector(`.exercises-list`);
 const exercisesListPages = document.querySelector(`.exercises-list-pages`);
+exercisesListPages.addEventListener(`click`, showsExercisesPages);
 exercisesList.addEventListener(`click`, makeTypeOfTrainingCards);
 const formCard = document.querySelector(`.exercises-form`);
+formCard.addEventListener(`submit`, searchCardsByKeyWord);
+const buttonList = document.querySelector(`.exercises-button-list`);
 let currentPage = 1,
   perPage = 8,
-  typeOfFilter = `Muscles`;
-
+  typeOfFilter = `Muscles`,
+  filterTypeCads = ``,
+  userTextSearch = ``;
+// document.body.clientWidth >= 768 ? (perPage = 12) : (perPage = 8);
 const filterButton = document.querySelectorAll(`.exercises-button`);
 [...filterButton].map((b, i) => {
   if (i === 0) b.classList.add(`exercises-button-active`);
@@ -22,8 +28,12 @@ async function searchImageOnServer(
   filterType,
   keyWord
 ) {
+  // ===================
+
+  console.log(perPage);
   // =====================
   let params = {};
+  if (filterType && filter == `Body parts`) filter = `Body part`;
   filterType
     ? (params[filter.toLowerCase().replace(` `, ``)] = filterType)
     : (params.filter = filter);
@@ -35,7 +45,9 @@ async function searchImageOnServer(
   params.page = currentPages;
   params.limit = perPages;
   if (keyWord) params.keyword = keyWord;
-
+  if (document.body.clientWidth >= 1440 && filterType) params.limit = 9;
+  if (document.body.clientWidth >= 768 && !filterType) params.limit = 12;
+  if (document.body.clientWidth >= 768 && filterType) params.limit = 8;
   console.log(params);
   // ========================
   let parameters = new URLSearchParams(params).toString();
@@ -44,14 +56,34 @@ async function searchImageOnServer(
     const response = await axios.get(
       `https://energyflow.b.goit.study/api/${getType}?${parameters}`
     );
+    console.log(response.data.results.length);
+    if (keyWord && response.data.results.length == 0) {
+      createMessagNoResult();
+      return;
+    }
     return response.data;
   } catch (error) {
     console.log(error);
   }
 }
+function createMessagNoResult() {
+  exercisesList.innerHTML = `<div class="no-results-wrapper">
+  <p class="no-results-message">
+    Unfortunately, <span class="no-results-messag-span">no results</span> no
+    results were found. You may want to consider other search options to find
+    the exercise you are looking for. Our range is wide and you have the
+    opportunity to find more options that suit your needs.
+  </p>
+</div>`;
+  exercisesListPages.innerHTML = ``;
+}
 
 filterExercises();
 function filterExercises(e) {
+  buttonList.classList.remove(`form-is-open`);
+  formCard.classList.add(`form-is-hiden`);
+  filterTypeCads = ``;
+  exercisesTitle.innerHTML = ``;
   currentPage = 1;
   if (e) {
     const button = e.target;
@@ -89,8 +121,8 @@ function renderExercises(arrow, totalPages) {
             <div class="card-workout-wrapper">
               <p class="exer-workout-text">WORKOUT</p>
               <p class="exer-card-rating">${rating}</p>
-              <svg class="card-rating-svg" width="14" height="14">
-                <use href="./img/symbol-defs.svg#icon-star"></use>
+              <svg class="card-rating-svg" width="16" height="16">
+                <use href="../img/symbol-defs.svg#icon-star"></use>
               </svg>
               <button class="card-start-button">
                 Start
@@ -121,22 +153,12 @@ function renderExercises(arrow, totalPages) {
       },
       ``
     );
-    //   bodyPart: 'lower legs';
-    //   burnedCalories: 42;
-    //   description: "Located on the back of the lower leg, the calves include the gastrocnemius and soleus muscles. They're responsible for plantar flexion (raising the heel). Calves are targeted in exercises like calf raises and during running and jumping.";
-    //   equipment: 'body weight';
-    //   gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/1387.gif';
-    //   name: 'one leg floor calf raise';
-    //   popularity: 76;
-    //   rating: 4.33;
-    //   target: 'calves';
-    //   time: 3;
-    //   _id: '64f389465ae26083f39b1af6';
     exercisesList.innerHTML = listCodeCards;
   }
   if (!arrow[0]._id) {
     let listCode = arrow.reduce(
       (list, { filter, imgUrl, name: nameMusculs }) => {
+        formCard.dataset.filter = filter;
         return (list += `<li class="exercises-item" data-type="${nameMusculs}" style="background-image: linear-gradient(
       0deg,
       rgba(16, 16, 16, 0.7) 0%,
@@ -154,22 +176,26 @@ function renderExercises(arrow, totalPages) {
   if (
     !exercisesListPages.textContent ||
     typeOfFilter !==
-      document.querySelector(`.exercises-pages-button`).dataset.type
-  )
+      document.querySelector(`.exercises-pages-button`).dataset.type ||
+    formCard.dataset.status
+  ) {
+    formCard.dataset.status = ``;
     addNumberOfPages();
+  }
+
   //   ======================================
   function addNumberOfPages() {
+    formCard.dataset.status = ``;
     let numberOfPages = ``;
     for (let i = 1; i < totalPages + 1; i++) {
       numberOfPages += `<li>
-          <button class="exercises-pages-button" data-number="${i}" data-type="${typeOfFilter}">${i}</button>
+          <button class="exercises-pages-button" data-card="${filterTypeCads}" data-number="${i}" data-type="${typeOfFilter}">${i}</button>
         </li>`;
     }
     exercisesListPages.innerHTML = numberOfPages;
     document
       .querySelector(`.exercises-pages-button`)
       .classList.add(`select-pages-ative`);
-    exercisesListPages.addEventListener(`click`, showsExercisesPages);
   }
   //   ========================================================================
 }
@@ -179,52 +205,60 @@ function showsExercisesPages(e) {
   pageNumber.classList.remove(`select-pages-ative`);
   e.target.classList.add(`select-pages-ative`);
   currentPage = e.target.dataset.number;
-  getExercisesFromServer(e.target.dataset.type);
+  if (filterTypeCads == ``) {
+    getExercisesFromServer(e.target.dataset.type);
+    return;
+  }
+  console.log(userTextSearch);
+  getCardsFromServer(
+    e.target.dataset.type,
+    e.target.dataset.card,
+    userTextSearch
+  );
 }
 // ==================================================================
 function makeTypeOfTrainingCards(e) {
-  let filter = document.querySelector(`.exercises-button-active`).dataset.name;
-  if (filter === `Body parts`) filter = `Body part`;
-  const filterType = e.target.dataset.type;
-  currentPage = 1;
-  formCard.classList;
-  getCardsFromServer(filter, filterType);
+  if (e.target.dataset.type) {
+    let filter = document.querySelector(`.exercises-button-active`).dataset
+      .name;
+    if (filter === `Body parts`) filter = `Body part`;
+    filterTypeCads = e.target.dataset.type;
+    currentPage = 1;
+    formCard.classList.remove(`form-is-hiden`);
+
+    exercisesListPages.innerHTML = ``;
+    buttonList.classList.add(`form-is-open`);
+
+    exercisesTitle.innerHTML = `<span class="exer-title-slash">/</span> ${filterTypeCads
+      .slice(0, 1)
+      .toUpperCase()}${filterTypeCads.slice(1)}`;
+
+    getCardsFromServer(filter, filterTypeCads);
+  }
 }
-async function getCardsFromServer(filter, filterType) {
+async function getCardsFromServer(filter, filterType, keytext = ``) {
   try {
     const answer = await searchImageOnServer(
       `exercises`,
       filter,
       currentPage,
       perPage,
-      filterType
+      filterType,
+      keytext
     );
-    console.log(answer);
-    renderExercises(answer.results, answer.totalPages);
+    if (answer) renderExercises(answer.results, answer.totalPages);
   } catch (error) {
     console.log(error);
   }
 }
-// function makeTypeOfTrainingCards(e) {
-//   exercisesList.removeEventListener(`click`, makeTypeOfTrainingCards);
-//   const filter = document.querySelector(`.exercises-button-active`).dataset
-//     .name;
-//   const filterType = e.target.dataset.type;
-//   currentPage = 1;
-//   getCardsFromServer();
-
-//   async function getCardsFromServer() {
-//     try {
-//       const answer = await searchImageOnServer(
-//         `exercises`,
-//         filter,
-//         currentPage,
-//         perPage,
-//         filterType
-//       );
-//       console.log(answer);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-// }
+function searchCardsByKeyWord(e) {
+  e.preventDefault();
+  userTextSearch = e.target.elements.usertext.value.trim();
+  if (userTextSearch.length !== 0) {
+    const filter = e.target.dataset.filter;
+    e.target.dataset.status = `ok`;
+    formCard.reset();
+    getCardsFromServer(filter, filterTypeCads, userTextSearch);
+    console.log(e.target);
+  }
+}
